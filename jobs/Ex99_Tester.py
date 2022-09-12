6626fb9d5cc520798a5954cf0f85f496dd8c686a
Drydale, Aaron <aadrjm@gmail.com>
2022-09-12 13:03:00 -0500

    Added testing job

---
+++ jobs/Ex99_Tester.py
@@ -0,0 +1,21 @@
+from nautobot.extras.jobs import Job
+
+# This is the job grouping within the Nautobot UI.
+name = "AD Example jobs"
+
+# This is the job being imported.
+class Ex99_Tester(Job):
+  # The Meta class within the job class is used for job extensible data
+  class Meta():
+    # This is what the job will be named in the UI.
+    name = "Example 99 - Tester"
+    # The first line of the description will be displayed but other lines will
+    # only be displayed on job details.
+    description = """
+      Example job for a simple "Hello World" output
+    """
+
+  def run(self, data, commit):
+    # This is a simple job that outputs "Hello world" to the job output/result
+    # data.
+    self.log_info("Test")
