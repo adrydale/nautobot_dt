fb0ac3a7a64a5790ab5e632fe379acf0ae46128b
Drydale, Aaron <adrydale@quiktrip.com>
2022-09-12 08:46:20 -0500

    Moved Ex01 out of subfolder.

---
+++ jobs/Ex01_HW.py
@@ -0,0 +1,10 @@
+from nautobot.extras.jobs import Job
+
+class Ex01_HelloWorld(Job):
+  def Meta():
+    name = "Example 01 - Hello World"
+    description = """
+      Example job for a simple "Hello World" output
+    """
+  def run(self, data, commit):
+    self.log("Hello world!")
