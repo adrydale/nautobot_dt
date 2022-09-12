0df3ce3b5e1d5b12de99fb084196a0f39e12b643
Drydale, Aaron <adrydale@quiktrip.com>
2022-09-12 09:10:39 -0500

    Added comments to init and ex01.

--- jobs/Ex01_HW.py
+++ jobs/Ex01_HW.py
@@ -1,12 +1,20 @@
 from nautobot.extras.jobs import Job
 
+# This is the job grouping within the Nautobot UI.
 name = "Example jobs"
 
+# This is the job being imported.
 class Ex01_HelloWorld(Job):
-  def Meta():
+  # The Meta class within the job class is used for job extensible data
+  class Meta():
+    # This is what the job will be named in the UI.
     name = "Example 01 - Hello World"
+    # The first line of the description will be displayed but other lines will
+    #   only be displayed on job details.
     description = """
       Example job for a simple "Hello World" output
     """
   def run(self, data, commit):
+    # This is a simple job that outputs "Hello world" to the job output/result
+    #   data.
     self.log("Hello world!")
--- jobs/__init__.py
+++ jobs/__init__.py
@@ -1,3 +1,8 @@
 # Example 01 - Hello world
+# The filename is Ex01_HW and the class/job is Ex01_HelloWorld. The difference
+#   between the filename and the class/job name is intentional to show import
+#   details
 from Ex01_HW import Ex01_HelloWorld
+
+# Example 02 - Inputs
 from Ex02_Inputs import Ex02_Inputs
