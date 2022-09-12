269cfd8bb2bb33fd15942eeb08f73c318bab7151
Drydale, Aaron <aadrjm@gmail.com>
2022-09-12 13:12:33 -0500

    Troubleshooting ex04

--- jobs/Ex04_AccessingSecrets.py
+++ jobs/Ex04_AccessingSecrets.py
@@ -1,4 +1,5 @@
 from nautobot.extras.jobs import Job
+from nautobot.extras.jobs import BooleanVar
 
 # These imports are the type of inputs that are being used in this job.
 #from nautobot.extras.models.secrets import Secret
@@ -14,6 +15,7 @@ class Ex04_AccessingSecrets(Job):
       Display the example secret in the job result/output. It is NOT recommended
       to do this for any actual secret. Job result data is stored and accessable
       to authorized users in plain text.
+
       Secrets were added in Nautobot version 1.2.0.
     """,
     label = "Display example shhh"
@@ -23,17 +25,17 @@ class Ex04_AccessingSecrets(Job):
   class Meta():
     # This is what the job will be named in the UI.
     name = "Example 04 - Accessing Secrets"
-    # The first line of the description will be displayed but other lines will
-    #   only be displayed on job details.
-    #description = """
-    #  This example shows how to access Nautobot secrets.
-    #
-    #  It is assumed that the Nautobot secret "example_secret_01" is already
-    #  setup within Nautobot. Please refer to the Nautobot documentation for
-    #  details on how to add a secret.
-    #
-    #  https://nautobot.readthedocs.io/en/stable/core-functionality/secrets/
-    #"""
+     The first line of the description will be displayed but other lines will
+       only be displayed on job details.
+    description = """
+      This example shows how to access Nautobot secrets.
+
+      It is assumed that the Nautobot secret "example_secret_01" is already
+      setup within Nautobot. Please refer to the Nautobot documentation for
+      details on how to add a secret.
+
+      https://nautobot.readthedocs.io/en/stable/core-functionality/secrets/
+    """
 
   def run(self, data, commit):
     # This job shows how to access Nautobot secrets.
