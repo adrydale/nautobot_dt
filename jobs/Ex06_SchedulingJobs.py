2828c4766d2da5e6a27182362d65c7e0bd1c521d
Aaron Drydale <aadrjm@gmail.com>
2022-09-20 15:41:00 -0500

    Added Ex06.

---
+++ jobs/Ex06_SchedulingJobs.py
@@ -0,0 +1,50 @@
+from . import common
+from nautobot.extras.jobs import Job
+
+# The datetime library gives us access to date and time functions.
+from datetime import datetime
+
+# This is the job grouping within the Nautobot UI.
+name = "Example Git Repo Jobs"
+
+# This is the job being imported. We include "Job" in the class definition
+# because our new class/Nautobot job will extend the built in functions that
+# Nautobot Jobs have.
+class Ex06_SchedulingJobs(Job):
+  # The Meta class within the job class is used for job extensible data
+  class Meta():
+    # This is what the job will be named in the UI.
+    name = "Example 06 - Scheduling Jobs"
+    # The first line of the description will be displayed but other lines will
+    # only be displayed on job details.
+    description = """
+      Example job for a job that can be scheduled.
+
+      Jobs by default will have has_sensitive_variables set to True. These jobs
+      cannot be scheduled. Changing has_sensitive_variables to False will enable
+      the job to be scheduled for the future once or on a recurring schedule.
+
+      **Important** Please ensure that the Nautobot time zone is set as you
+      expect when scheduling jobs. The current Nautobot time is located at the
+      bottom of every web page.
+    """
+
+    # This is the variable that we'll set to False so that the job can be
+    # scheduled. It's important to understand that this option is defaulted to
+    # True for a reason. Be careful when setting this to False and be sure to
+    # understand the implications
+    # https://nautobot.readthedocs.io/en/latest/additional-features/jobs/
+    has_sensitive_variables = False
+
+  def run(self, data, commit):
+    self.log_info("Example job for scheduling.")
+
+    # This job isn't intended to do anything else. In the web GUI, when running
+    # this job a "Job Execution" section will be added allowing the user to
+    # choose when the job is run. By default, the options are:
+    #  - Once immediately
+    #  - Once in the future
+    #  - Recurring hourly
+    #  - Recurring daily
+    #  - Recurring weekly
+    #  - Recurring custom (crontab syntax)
